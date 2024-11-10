27504ff39abd9da378913cd9b481f485a8e2c74e
soldiereyes <guilhermegonzagasoares_@hotmail.com>
2024-11-10 00:04:23 -0300

    feat: criação da ClientService com as operações iniciais de CRUD

---
+++ src/main/java/com/sisgcobrancas/sisgcobrancas/service/ClientService.java
@@ -0,0 +1,46 @@
+package com.sisgcobrancas.sisgcobrancas.service;
+
+import com.sisgcobrancas.sisgcobrancas.model.Client;
+import com.sisgcobrancas.sisgcobrancas.repository.ClientRepository;
+import com.sisgcobrancas.sisgcobrancas.exceptions.ClientNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class ClientService {
+
+    @Autowired
+    private ClientRepository clientRepository;
+
+    public Client saveClient(Client client) {
+        return clientRepository.save(client);
+    }
+
+    public Client getClientById(UUID id) {
+        return clientRepository.findById(id)
+                .orElseThrow(() -> new ClientNotFoundException("Client not found with id: " + id));
+    }
+
+    public List<Client> getAllClients() {
+        return clientRepository.findAll();
+    }
+
+    public Client updateClient(UUID id, Client clientDetails) {
+        return clientRepository.findById(id)
+                .map(client -> {
+                    client.setCnpj(clientDetails.getCnpj());
+                    client.setCpf(clientDetails.getCpf());
+                    client.setEmail(clientDetails.getEmail());
+                    client.setNome(clientDetails.getNome());
+                    return clientRepository.save(client);
+                })
+                .orElseThrow(() -> new RuntimeException("Client not found with id: " + id));
+    }
+
+    public void deleteClient(UUID id) {
+        clientRepository.deleteById(id);
+    }
+}
