4e8c631e770ea4602fb9ec36bd05067c395c2041
soldiereyes <guilhermegonzagasoares_@hotmail.com>
2024-11-09 21:29:58 -0300

    create: criação do projeto e inicialização

---
+++ src/main/java/com/sisgcobrancas/sisgcobrancas/SisgcobrancasApiApplication.java
@@ -0,0 +1,13 @@
+package com.sisgcobrancas.sisgcobrancas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisgcobrancasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisgcobrancasApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sisgcobrancas/sisgcobrancas/SisgcobrancasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sisgcobrancas.sisgcobrancas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisgcobrancasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
